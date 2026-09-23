1d7933736d79d8041f236e348bc15432ffb3a3f5
dAIm0on <kristina.starykh@gmail.com>
2026-09-24 02:16:31 +0500

    test: expect trainer catalog service worker cache

--- verify_personal_trainers.cjs
+++ verify_personal_trainers.cjs
@@ -129,7 +129,7 @@ test('K2.1-16 service worker caches both standalone trainer files',()=>{
   const sw=fs.readFileSync(path.join(__dirname,'sw.js'),'utf8');
   assert.ok(sw.includes('"harmony-letter-trainer.js"'));
   assert.ok(sw.includes('"personal-trainers.js"'));
-  assert.ok(sw.includes('20260923-cat-trainer'));
+  assert.ok(sw.includes('20260924-trainer-catalog'));
 });
 
 
